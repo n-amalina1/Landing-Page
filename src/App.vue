2e0bb4d1e618b2c82df1cf4e96703939a6794366
Amalina <n.amalina@anxingtech.com>
2022-03-15 16:32:12 +0800

    Added navbar and header

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,53 @@
 <template>
-  <Home />
+  <v-app>
+    <v-app-bar app>
+      <nav class="navbar navbar-inverse navbar-fixed-top">
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <a class="navbar-brand" href="#">SGUnited</a>
+          </div>
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#">Programs</a></li>
+            <li><a href="#">About Us</a></li>
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+            <li>
+              <a href="#"
+                ><span class="glyphicon glyphicon-user"></span> Sign Up</a
+              >
+            </li>
+            <li>
+              <a href="#"
+                ><span class="glyphicon glyphicon-log-in"></span> Login</a
+              >
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </v-app-bar>
+    <v-main>
+      <Header />
+      <Home />
+    </v-main>
+
+    <v-footer app> </v-footer>
+  </v-app>
 </template>
 
 <script>
-import Home from './components/Home.vue'
+import Header from "./components/Header.vue";
+import Home from "./components/Home.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    Home
-  }
-}
+    Header,
+    Home,
+  },
+};
 </script>
+
 
 
 
---
+++ src/components/Header.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="page-header">
+    <div class="container">
+      <div class="row">
+        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+          <div class="page-caption">
+            <h1 class="page-title">SGUnited Traineeship Programmes</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="program-bar">
+    <div class="container">
+      <div class="text">
+        Join Our Programs
+        <button type="button" class="btn btn-default">Find Out More</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style scoped>
+body {
+  -webkit-font-smoothing: antialiased;
+  text-rendering: optimizeLegibility;
+  font-family: "Overpass", sans-serif;
+  letter-spacing: 0px;
+  font-size: 17px;
+  color: #8d8f90;
+  font-weight: 400;
+  line-height: 32px;
+  background-color: #edefef;
+}
+
+a:focus,
+a:hover {
+  text-decoration: none;
+  color: #f85759;
+}
+
+.page-header {
+  background: url(https://easetemplate.com/free-website-templates/hike/images/pageheader.jpg)
+    no-repeat;
+  position: relative;
+  background-size: cover;
+}
+.page-caption {
+  padding-top: 170px;
+  padding-bottom: 174px;
+}
+.page-title {
+  font-size: 46px;
+  line-height: 1;
+  color: #fff;
+  font-weight: 600;
+  text-align: center;
+}
+
+.program-bar {
+  padding: 1.75rem 0;
+  font-size: 1.25em;
+  background-color: #0074b9 !important;
+  color: #fff !important;
+  text-align: center;
+}
+
+.btn{
+    margin-left: 50px;
+    background-color: #fff;
+    color: #0074b9!important;
+    line-height: 40px;
+    font-size: 15px;
+    padding: 0 30px;
+}
+</style>
