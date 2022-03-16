f4aeac5d54cb416cd8eb708bb9ada8c2909ab7d5
Amalina <n.amalina@anxingtech.com>
2022-03-16 11:11:45 +0800

    Added button to make image appear

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,47 +1,59 @@
 <template>
-  <div class="page-header">
-    <div class="container">
-      <div class="row">
-        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-          <div class="page-caption">
-            <h1 class="page-title animate__animated animate__bounce animate__infinite">Welcome</h1>
-            <h2 class="page-title2 animate__animated animate__bounce animate__infinite">SGUnited</h2>
-          </div>
+  <div class="page-header" id="header">
+    <div class="row">
+      <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+        <div class="page-caption">
+          <h1
+            class="
+              page-title
+              animate__animated animate__bounce animate__infinite
+            "
+          >
+            Welcome
+          </h1>
+          <h2
+            class="
+              page-title2
+              animate__animated animate__bounce animate__infinite
+            "
+          >
+            SGUnited
+          </h2>
+          <button
+            @click="showImage"
+            type="button"
+            class="btn btn-outline-light"
+          >
+            Enter
+          </button>
         </div>
       </div>
     </div>
   </div>
-  <!--
-  <div class="program-bar">
-    <div class="container">
-      <div class="text">
-        Join Our Programs
-        <button type="button" class="btn btn-default">Find Out More</button>
-      </div>
-    </div>
+  <div id="photo" class="photo" v-if="showPhoto" >
+    <img class="responsive" :src="image"/>
   </div>
-  -->
 </template>
 
 <script>
 export default {
   name: "Header",
+  data() {
+    return {
+      showPhoto: false,
+      image: require("@/assets/SgUnited2.jpeg"),
+    };
+  },
+  methods: {
+    showImage() {
+      document.getElementById("header").style.display = "none";
+      this.showPhoto = true;
+    },
+  },
 };
 </script>
-``
-<style scoped>
-body {
-  -webkit-font-smoothing: antialiased;
-  text-rendering: optimizeLegibility;
-  font-family: "Overpass", sans-serif;
-  letter-spacing: 0px;
-  font-size: 17px;
-  color: #8d8f90;
-  font-weight: 400;
-  line-height: 32px;
-  background-color: #edefef;
-}
 
+<style scoped>
 a:focus,
 a:hover {
   text-decoration: none;
@@ -57,6 +69,7 @@ a:hover {
 .page-caption {
   padding-top: 170px;
   padding-bottom: 174px;
+  text-align: center;
 }
 .page-title {
   font-size: 10vw;
@@ -73,20 +86,22 @@ a:hover {
   text-align: center;
 }
 
-.program-bar {
-  padding: 1.75rem 0;
-  font-size: 1.25em;
-  background-color: #0074b9 !important;
-  color: #fff !important;
-  text-align: center;
+.btn {
+  line-height: 40px;
+  font-size: 20px;
+  padding: 0 30px;
+  margin-top: 20px;
+}
+.responsive{
+  padding-top: 50px;
+  width: 75%;
+  height: auto;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+}
+.responsive:hover{
+  width: 90%;
 }
 
-.btn{
-    margin-left: 50px;
-    background-color: #fff;
-    color: #0074b9!important;
-    line-height: 40px;
-    font-size: 15px;
-    padding: 0 30px;
-}
 </style>
